217da0cbef5b3783026eae882ea9d6d2dd445cb2
Glitch (hello-webpage) <none>
2020-07-08 16:13:30 +0000

    👷👩‍👩‍👦 Checkpoint ./solutions2.js:15951174/8997 ./script.js:15951174/2169 ./index.html:15951174/70

--- script.js
+++ script.js
@@ -27,52 +27,61 @@
 /* global
  *    HSB,
  *    background,
- *    circle,
  *    color,
  *    colorMode,
  *    createCanvas,
  *    ellipse,
  *    fill,
- *    height,
  *    line,
- *    mouseX,
- *    mouseY,
  *    noStroke,
- *    resizeCanvas,
  *    stroke,
  *    text,
- *    width,
- *    windowHeight,
- *    windowWidth,
  */
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
+let backgroundColor, color1, color2, textColor;
 
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+function setup() {
+  // Canvas & color settings
+  createCanvas(400, 400);
+  colorMode(HSB, 360, 100, 100);
+  noStroke();
+
+  // When used with only one argument, the color mode is greyscale.
+  // 0 is black and 100 is white.
+  backgroundColor = color(95);
+  textColor = color(20);
+  // When used with three arguments, the function takes, in this order:
+  // HUE - 0 to 360 degrees on a color wheel - 0 is red, 120 is green and 240
+  //       is blue.
+  // SATURATION - 0 is no color (greyscale), and 100 is as bold as possible.
+  // BRIGHTNESS - 0 is no light (black), and 100 is as bright as possible.
+  color1 = color(0, 80, 80);
+  color2 = color(200, 80, 80);
 }
 
 function draw() {
-  let isDarkTheme = mouseX > width / 2;
+  background(backgroundColor);
+  // Call the drawCenterLine function here to run the three lines of code
+  // contained in that function.
 
-  background(isDarkTheme ? 50 : 205);
-  
-  stroke(isDarkTheme ? 205 : 50);
-  line(width / 2, 0, width / 2, height);
-  
-  fill(isDarkTheme ? 205 : 50);
-  text('Flip the switch', 10, 20);
-  
+
+  // The red and blue circles:
+  fill(color1);
+  ellipse(100, 200, 50);
+  fill(color2);
+  ellipse(300, 200, 50);
+
+  // The grey circle and the text:
+  fill(textColor);
+  ellipse(50, 50, 50);
+  text("Flip the switch", 20, 20);
+}
+
+function drawCenterLine() {
+  // This function will turn stroke on, draw the line, and then turn stroke
+  // back off.
+  // Remember a line segment in p5.js has four arguments: x1, y1, x2, y2
+  stroke(textColor);
+  line(200, 0, 200, 400);
   noStroke();
-  
-  fill(isDarkTheme ? 'blue' : 'red');
-  circle(width / 4, height / 2, width / 10);
-  
-  fill(isDarkTheme ? 'red' : 'blue');
-  circle(3 * width / 4, height / 2, width / 10);
-  
-  fill(isDarkTheme ? 205 : 50);
-  circle(mouseX, mouseY, width / 10);
 }
--- solutions.js
+++ solutions.js
@@ -70,62 +70,33 @@
  *    windowWidth,
  */
 
-let backgroundColor, color1, color2, textColor, globalS, globalB
-
 function setup() {
-  createCanvas(400, 400);
-  colorMode(HSB, 360, 100, 100);
-  noStroke();
-  // Set saturation and brightness globally so we can experiment with
-  // different color palettes as we go without having to change multiple things
-  globalS = 90;
-  globalB = 90;
-  // We'll keep background color in a variable so we can modify it in
-  // other functions.
-  backgroundColor = color(95);
-  // The variable color1 will be used for the ellipse on the left, and color2
-  // will be the ellipse on the right.
-  color1 = color(0, globalS, globalB);
-  color2 = color(200, globalS, globalB);
-  // We will use this textColor variable as a way to make sure the text is
-  // always visible against a changing background.
-  textColor = color(20);
+  createCanvas(windowWidth, windowHeight);
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(backgroundColor)
-  drawCenterLine();
-  if (mouseX > width / 2) {
-    nightMode();
-  } else {
-    dayMode();
-  }
-  fill(color1);
-  ellipse(1/4 * width, height / 2, 50);
-  fill(color2);
-  ellipse(3/4 * width, height / 2, 50);
-  fill(textColor);
-  text("Flip the switch", 20, 20);
-  ellipse(mouseX, mouseY, 50);
-}
+  let isDarkTheme = mouseX > width / 2;
 
-function drawCenterLine() {
-  // Turn stroke on, draw the line, turn stroke back off.
-  stroke(textColor);
+  background(isDarkTheme ? 50 : 205);
+  
+  stroke(isDarkTheme ? 205 : 50);
   line(width / 2, 0, width / 2, height);
+  
+  fill(isDarkTheme ? 205 : 50);
+  text('Flip the switch', 10, 20);
+  
   noStroke();
-}
-
-function dayMode() {
-  backgroundColor = color(95);
-  color1 = color(0, globalS, globalB);
-  color2 = color(200, globalS, globalB);
-  textColor = color(20);
-}
-
-function nightMode() {
-  backgroundColor = color(20);
-  color1 = color(200, globalS, globalB);
-  color2 = color(0, globalS, globalB);
-  textColor = color(95);
+  
+  fill(isDarkTheme ? 'blue' : 'red');
+  circle(width / 4, height / 2, width / 10);
+  
+  fill(isDarkTheme ? 'red' : 'blue');
+  circle(3 * width / 4, height / 2, width / 10);
+  
+  fill(isDarkTheme ? 205 : 50);
+  circle(mouseX, mouseY, width / 10);
 }
